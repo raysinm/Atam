23dd653c6ae7cb4cb367116c2731313da7cfc414
stavtoledo <85308470+stavtoledo@users.noreply.github.com>
2022-04-11 23:25:39 +0300

    no dots for the segel

--- hw1_final/ex4.asm
+++ hw1_final/ex4.asm
@@ -2,46 +2,45 @@
 
 .section .text
 _start:
-pos1:
+
 movq new_node, %r8          #copy new_node val to reg
 movq (root), %rbx          #copy root address to reg
-pos2:
 movq %rbx, %r9          #move rbx address to reg	
 test %r9, %r9
-jz .insert_new_node_root_HW1        #jump to insert the node if root is 0
-jmp .main_loop_HW1
+jz insert_new_node_root_HW1        #jump to insert the node if root is 0
+jmp main_loop_HW1
 
-.main_loop_HW1:
+main_loop_HW1:
    movq (%rbx), %r12          #move rbx value to reg
    movq 8(%rbx), %r10        #r10 keeps left son
    movq 16(%rbx), %r11        #r11 keeps right son
 	cmp %r12, %r8
-	je .end_HW1			#jump to end if val found in tree
-	ja .go_right_HW1		#jump right if bigger than node
-	jb .go_left_HW1			#jump left if bigger than node
+	je end_HW1			#jump to end if val found in tree
+	ja go_right_HW1		#jump right if bigger than node
+	jb go_left_HW1			#jump left if bigger than node
 
-.go_left_HW1:
+go_left_HW1:
    test %r10, %r10
-   jz .insert_new_node_left_HW1
+   jz insert_new_node_left_HW1
 	movq 8(%rbx), %rbx        #put left son address into %rbx
-	jmp .main_loop_HW1
+	jmp main_loop_HW1
 	
-.go_right_HW1:
+go_right_HW1:
    test %r11, %r11
-   jz .insert_new_node_right_HW1
+   jz insert_new_node_right_HW1
 	movq 16(%rbx), %rbx        #put right son address into %rbx
-	jmp .main_loop_HW1
+	jmp main_loop_HW1
 
-.insert_new_node_root_HW1:
+insert_new_node_root_HW1:
     movq $new_node, (root)
-    jmp .end_HW1
+    jmp end_HW1
     	
-.insert_new_node_left_HW1:
+insert_new_node_left_HW1:
     movq $new_node, 8(%rbx)
-    jmp .end_HW1
+    jmp end_HW1
 
-.insert_new_node_right_HW1:
+insert_new_node_right_HW1:
     movq $new_node, 16(%rbx)
-    jmp .end_HW1
+    jmp end_HW1
 
-.end_HW1:
+end_HW1:
