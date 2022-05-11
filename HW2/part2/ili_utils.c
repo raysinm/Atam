a7368a5d6f5b6e2a454ba18617f878971e97d6d2
stavtoledo <85308470+stavtoledo@users.noreply.github.com>
2022-05-11 09:40:37 +0300

    done! now DEBUG

--- HW2/part2/ili_utils.c
+++ HW2/part2/ili_utils.c
@@ -26,14 +26,15 @@ void my_load_idt(struct desc_ptr *idtr) {
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
 // <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
-
-
+	gate->offset_low = addr;
+    gate->offset_middle = addr >> 16;
+    gate->offset_high = addr >> 32;
 // </STUDENT FILL>
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
 // <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
+	return gate->offset_low | ((unsigned long)gate->offset_middle << 16) |
+		((unsigned long) gate->offset_high << 32);
 // </STUDENT FILL>
 }
